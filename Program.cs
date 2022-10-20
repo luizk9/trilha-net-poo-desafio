34bc17ff7314ba072894e179f6de3157bd168055
EDIMIR LUIZ <76841015+luizk9@users.noreply.github.com>
2022-10-20 15:09:50 -0300

    instanciando class Nokia e Iphone
    executando os métodos das classes filhas e subscrevendo os do pai..

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+using DesafioPOO.Models;
+
+
+// Etapa pronta do --->  TODO: Realizar os testes com as classes Nokia e Iphone
+
+// inicializando a classe Nokia
+Nokia n = new Nokia("9999", "nsnns", "111", 2323);
+
+n.InstalarAplicativo("nokia");
+n.Ligar();
+
+
+// instanciando a class Iphone
+Iphone ip = new Iphone("2222", "Modiphone", "34Ip", 88989);
+
+ip.InstalarAplicativo("Iphone");
+ip.ReceberLigacao();
