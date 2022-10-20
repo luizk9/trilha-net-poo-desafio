797aecc80132c8ec4a48dd26bb414de49f035cd0
EDIMIR LUIZ <76841015+luizk9@users.noreply.github.com>
2022-10-20 15:01:00 -0300

    classe Iphone etapa 1
    Nesta etapa, foi  herdado da classe Pai, e subscrito o método Instalar.

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,24 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // etapa pronta do -- > TODO: Herdar da classe "Smartphone"
+    public class Iphone  : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+          public Iphone(string numero, string modelo, string imei, int memoria) : base (numero, modelo,imei,memoria)
+        {
+
+        }
+        //  etapa pronta do -- >  TODO: Sobrescrever o método "InstalarAplicativo"
+
+         public override void InstalarAplicativo(string nomeApp)
+        {
+                   Console.WriteLine();  
+             Console.WriteLine( $"Vamos instalar o {nomeApp}: {Numero}");
+             Console.WriteLine( "***** = = = =  ******" );
+             Console.WriteLine( $"---EMAI: {Imei}");
+             Console.WriteLine( $"---MODELO: {Modelo}");
+             Console.WriteLine( $"---EMEI: {Memoria}");
+             Console.WriteLine( $"---------------------....");
+        
+        }
     }
 }
